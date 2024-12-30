153aa6d6c495cc25cea28498bae94bd98dcf9d8f
Mubassir Habib <66573095+Mubassir1820@users.noreply.github.com>
2024-12-31 01:50:55 +0600

    Implementations of GET for dynamic parameter

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@
 
 from fastapi import FastAPI
 from core.config import settings
+from fastapi.responses import JSONResponse
+from fastapi import status
 
 
 
@@ -18,3 +20,13 @@ app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 @app.get("/books")
 def get_all_books():
     return books
+
+
+@app.get("/books/{book_id}")
+def get_single_book(book_id: int):
+    
+    for book in books:
+        if book['id'] == book_id:
+            return book
+
+    return JSONResponse(content={"Error!":"Book is not available!"}, status_code=status.HTTP_404_NOT_FOUND)
